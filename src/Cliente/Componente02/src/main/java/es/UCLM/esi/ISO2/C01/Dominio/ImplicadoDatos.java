18b8ce233655dfd89ee8031a9b14b0a81f59818b
KevinRuedas183 <Kevin.Ruedas@alu.uclm.es>
2024-11-16 17:10:57 +0100

    Componente 2. Cliente QUEDA PROGRAMADO, fixes #10

---
+++ src/Cliente/Componente02/src/main/java/es/UCLM/esi/ISO2/C01/Dominio/ImplicadoDatos.java
@@ -0,0 +1,17 @@
+package es.UCLM.esi.ISO2.C01.Dominio;
+
+public class ImplicadoDatos {
+
+	private String DNI;
+	private String NombreYApellidos;
+
+	/**
+	 * 
+	 * @param mapa
+	 */
+	public void proponerDatos(MapaActivosDatos mapa) {
+		// TODO - implement ImplicadoDatos.proponerDatos
+		throw new UnsupportedOperationException();
+	}
+
+}
