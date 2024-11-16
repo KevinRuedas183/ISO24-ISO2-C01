8ece4bfbed1b3e2f505521fc2d31ec4ff0382b70
KevinRuedas183 <Kevin.Ruedas@alu.uclm.es>
2024-11-16 13:42:32 +0100

    Componente 1, Servidor, QUEDA PROGRAMADO fixes #29

---
+++ src/Servidor/Componente01/src/main/java/es/UCLM/esi/ISO2/C01/Dominio/ProcesoDeNegocio.java
@@ -0,0 +1,27 @@
+package es.UCLM.esi.ISO2.C01.Dominio;
+
+public class ProcesoDeNegocio {
+
+	private String identificador;
+	private MapaActivosDatos[] mapas;
+
+	/**
+	 * 
+	 * @param identificador
+	 * @param activos
+	 */
+	public void generarMapa(String identificador, int[] activos) {
+		// TODO - implement ProcesoDeNegocio.generarMapa
+		throw new UnsupportedOperationException();
+	}
+
+	/**
+	 * 
+	 * @param identificador
+	 */
+	public MapaActivosDatos getMapa(String identificador) {
+		// TODO - implement ProcesoDeNegocio.getMapa
+		throw new UnsupportedOperationException();
+	}
+
+}
