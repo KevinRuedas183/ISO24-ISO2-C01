68265df90f55412dbeb127bf962fd854be0e38f2
Samuel Rodero Ballesteros <samuel.rodero@alu.uclm.es>
2024-11-20 17:21:35 +0100

    Fin Componente07 Servidor, fixes #15

---
+++ src/Cliente/Componente07/src/main/java/es/UCLM/esi/ISO2/C01/Dominio/PropietarioDatos.java
@@ -0,0 +1,13 @@
+package es.UCLM.esi.ISO2.C01.Dominio;
+public class PropietarioDatos {
+
+	/**
+	 * 
+	 * @param d
+	 */
+	public void CategorizarDatos(Datos d) {
+		// TODO - implement PropietarioDatos.CategorizarDatos
+		throw new UnsupportedOperationException();
+	}
+
+}
