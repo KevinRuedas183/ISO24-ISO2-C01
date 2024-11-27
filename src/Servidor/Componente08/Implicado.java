6ddcc784636f74030d82d6a5cdc5592cb9f67a2d
Rospide <alejandro.rospide@alu.uclm.es>
2024-11-27 16:43:45 +0100

    Componente8 del servidor finalizado, fixes #36

---
+++ src/Servidor/Componente08/Implicado.java
@@ -0,0 +1,48 @@
+public class Implicado {
+
+	private int id;
+	private string nombre;
+	private string rol;
+
+	public int getId() {
+		return this.id;
+	}
+
+	/**
+	 * 
+	 * @param id
+	 */
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public string getNombre() {
+		return this.nombre;
+	}
+
+	/**
+	 * 
+	 * @param nombre
+	 */
+	public void setNombre(string nombre) {
+		this.nombre = nombre;
+	}
+
+	public string getRol() {
+		return this.rol;
+	}
+
+	/**
+	 * 
+	 * @param rol
+	 */
+	public void setRol(string rol) {
+		this.rol = rol;
+	}
+
+	public void actualizar() {
+		// TODO - implement Implicado.actualizar
+		throw new UnsupportedOperationException();
+	}
+
+}
