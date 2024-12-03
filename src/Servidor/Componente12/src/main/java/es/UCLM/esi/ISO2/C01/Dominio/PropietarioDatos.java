59a12bfc3037151441561d4af11316390bebfb1e
Raul <Raul.Santiago1@uclm.es>
2024-12-03 01:45:27 +0100

    Componente12 del Servidor finalizado, fixes #40

---
+++ src/Servidor/Componente12/src/main/java/es/UCLM/esi/ISO2/C01/Dominio/PropietarioDatos.java
@@ -0,0 +1,23 @@
+public class PropietarioDatos {
+
+	ResultadoFlujo Resultado;
+	private int id;
+	private string nombre;
+	Sistema Sistema;
+
+	public void aprobarResultado() {
+		// TODO - implement PropietarioDatos.aprobarResultado
+		throw new UnsupportedOperationException();
+	}
+
+	public void rechazarResultado() {
+		// TODO - implement PropietarioDatos.rechazarResultado
+		throw new UnsupportedOperationException();
+	}
+
+	public void solicitarResultados() {
+		// TODO - implement PropietarioDatos.solicitarResultados
+		throw new UnsupportedOperationException();
+	}
+
+}
