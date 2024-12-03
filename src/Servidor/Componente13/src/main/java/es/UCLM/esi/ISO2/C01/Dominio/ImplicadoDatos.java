6c3b42c761486e49bad032b7bd9f5fcc50a57c0e
Raul <Raul.Santiago1@uclm.es>
2024-12-03 01:37:37 +0100

    Componente13 del Servidor finalizado, fixes #41

---
+++ src/Servidor/Componente13/src/main/java/es/UCLM/esi/ISO2/C01/Dominio/ImplicadoDatos.java
@@ -0,0 +1,13 @@
+public class ImplicadoDatos {
+
+	SolicitudInforme Solicitudes;
+	private int id;
+	private string nombre;
+	SolicitudInforme Solicitud;
+
+	public void solicitarInforme() {
+		// TODO - implement ImplicadoDatos.solicitarInforme
+		throw new UnsupportedOperationException();
+	}
+
+}
