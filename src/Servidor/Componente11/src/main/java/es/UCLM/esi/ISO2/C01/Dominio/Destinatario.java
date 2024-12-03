6d7b65edefaab8ef5b9ce26df1728c59d7911a0d
Raul <Raul.Santiago1@uclm.es>
2024-12-03 10:39:11 +0100

    Componente11 del servidor finalizado, fixes #39

---
+++ src/Servidor/Componente11/src/main/java/es/UCLM/esi/ISO2/C01/Dominio/Destinatario.java
@@ -0,0 +1,14 @@
+public class Destinatario {
+
+	GestorCalidad envia;
+	private int id;
+	private string email;
+	private string nombre;
+	Reporte recibidoPor;
+
+	public void recibirReporte() {
+		// TODO - implement Destinatario.recibirReporte
+		throw new UnsupportedOperationException();
+	}
+
+}
---
+++ src/Servidor/Componente11/src/main/java/es/UCLM/esi/ISO2/C01/Dominio/GestorCalidad.java
@@ -0,0 +1,31 @@
+public class GestorCalidad {
+
+	MapaDatos mapasGenerados;
+	Reporte ReportesPersonalizados;
+	Destinatario destinatarios;
+	private int id;
+	private string nombre;
+	Reporte envia;
+	Sistema Sistema;
+
+	public void generarReporte() {
+		// TODO - implement GestorCalidad.generarReporte
+		throw new UnsupportedOperationException();
+	}
+
+	public void personalizarReporte() {
+		// TODO - implement GestorCalidad.personalizarReporte
+		throw new UnsupportedOperationException();
+	}
+
+	public void enviarReporte() {
+		// TODO - implement GestorCalidad.enviarReporte
+		throw new UnsupportedOperationException();
+	}
+
+	public void actualizarSistema() {
+		// TODO - implement GestorCalidad.actualizarSistema
+		throw new UnsupportedOperationException();
+	}
+
+}
