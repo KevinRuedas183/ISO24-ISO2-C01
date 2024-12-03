c96f14ba4e5f3aeaee225b232203f38b70f9f454
Raul <Raul.Santiago1@uclm.es>
2024-12-03 01:41:29 +0100

    Componente12 del Cliente finalizado, fixes #20

---
+++ src/Cliente/Componente12/src/main/java/es/UCLM/esi/ISO2/C01/Dominio/FlujoTrabajo.java
@@ -0,0 +1,13 @@
+public class FlujoTrabajo {
+
+	ResultadoFlujo Resultado;
+	private int id;
+	private string nombre;
+	private List<ResultadoFlujo> resultados;
+
+	public void obtenerResultados() {
+		// TODO - implement FlujoTrabajo.obtenerResultados
+		throw new UnsupportedOperationException();
+	}
+
+}
---
+++ src/Cliente/Componente12/src/main/java/es/UCLM/esi/ISO2/C01/Dominio/PropietarioDatos.java
@@ -0,0 +1,17 @@
+public class PropietarioDatos {
+
+	ResultadoFlujo Resultado;
+	private int id;
+	private string nombre;
+
+	public void aprobarResultado() {
+		// TODO - implement PropietarioDatos.aprobarResultado
+		throw new UnsupportedOperationException();
+	}
+
+	public void rechazarResultado() {
+		// TODO - implement PropietarioDatos.rechazarResultado
+		throw new UnsupportedOperationException();
+	}
+
+}
