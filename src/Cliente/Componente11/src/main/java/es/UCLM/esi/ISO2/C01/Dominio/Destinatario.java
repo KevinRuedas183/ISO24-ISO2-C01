2ccd7abb72a1985fbca74378259082df5f8fe4f8
Raul <Raul.Santiago1@uclm.es>
2024-12-03 10:35:05 +0100

    Componente11 del cliente finalizado, fixes #19

---
+++ src/Cliente/Componente11/src/main/java/es/UCLM/esi/ISO2/C01/Dominio/Destinatario.java
@@ -0,0 +1,13 @@
+public class Destinatario {
+
+	GestorCalidad envia;
+	private int id;
+	private string email;
+	private string nombre;
+
+	public void recibirReporte() {
+		// TODO - implement Destinatario.recibirReporte
+		throw new UnsupportedOperationException();
+	}
+
+}
---
+++ src/Cliente/Componente11/src/main/java/es/UCLM/esi/ISO2/C01/Dominio/Reporte.java
@@ -0,0 +1,13 @@
+public class Reporte {
+
+	GestorCalidad personaliza;
+	private int id;
+	private string titulo;
+	private string contenido;
+
+	public void exportarFormato() {
+		// TODO - implement Reporte.exportarFormato
+		throw new UnsupportedOperationException();
+	}
+
+}
