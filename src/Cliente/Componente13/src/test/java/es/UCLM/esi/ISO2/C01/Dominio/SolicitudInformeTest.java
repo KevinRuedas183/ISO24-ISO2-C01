ee522dc3f6821dd3777628810f4a1354e897e3b2
Jaime Sesmero Verbo <Jaime.Sesmero@alu.uclm.es>
2024-12-03 13:34:36 +0100

    Pruebas del componente 13 del cliente

---
+++ src/Cliente/Componente13/src/test/java/es/UCLM/esi/ISO2/C01/Dominio/SolicitudInformeTest.java
@@ -0,0 +1,19 @@
+ppackage es.UCLM.esi.ISO2.C01.Dominio;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class SolicitudInformeTest {
+
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue() {
+        assertTrue(true);
+    }
+}
