4973e6669eb19adf7695c7cfb8d27055847dd0ba
Jaime Sesmero Verbo <Jaime.Sesmero@alu.uclm.es>
2024-12-06 11:55:44 +0100

    Componente 16 cliente

---
+++ src/Cliente/Componente16/src/main/java/es/UCLM/esi/ISO2/C01/Dominio/Gestor_Usuario.java
@@ -0,0 +1,21 @@
+public class Gestor_Usuario {
+
+	/**
+	 * 
+	 * @param u
+	 */
+	public void Agregar_Usuario(Usuario u) {
+		// TODO - implement Gestor_Usuario.Agregar_Usuario
+		throw new UnsupportedOperationException();
+	}
+
+	/**
+	 * 
+	 * @param u
+	 */
+	public void Log_usuario(Usuario u) {
+		// TODO - implement Gestor_Usuario.Log_usuario
+		throw new UnsupportedOperationException();
+	}
+
+}
